928b199579e58bc1653876f857701e7a1603ec0b
Ali D <ali@phinion.com>
2024-01-19 18:05:34 +0100

    Locator selectors

--- demo-test/tests/firstTest.spec.ts
+++ demo-test/tests/firstTest.spec.ts
@@ -3,26 +3,36 @@ import { test } from "@playwright/test";
 test.beforeEach(async({page})=>{
     await page.goto("http://localhost:4200/"); // commented lines will be first executed here 
     await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
 })
-test("the first test", async ({ page }) => {
-//   await page.goto("http://localhost:4200/"); 
-//   await page.getByText('Forms').click()
-  await page.getByText('Form Layouts').click()
-});
 
-test("navigate datepicker page", async ({ page }) => {
-    // await page.goto("http://localhost:4200/");
-    // await page.getByText('Forms').click()
-    await page.getByText('Datepicker').click()
-  });
+test('Locator syntax rules',async({page})=>{
 
+    //by Tag name
+    page.locator('input')
 
-//Grouped tests
-// test.describe('test suite1',()=>{
-//     test('the first test',()=>{
+    //by ID
+    await page.locator('#inputEmail1').click()
 
-//     })
-//     test('the  test2',()=>{
+    //by CLASS
+    page.locator('.shape-rectangle')
 
-//     })
-// })
+    //by attribute
+    page.locator('[placeholder="Email"]')
+
+    //by Class full value
+    page.locator('class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    //combine different selectors
+    page.locator('input[placeholder="Email"][nbinput]') // not add space between element and attribute
+
+    //by XPath (NOT RECOMMENDED)
+    page.locator('//*[@id="inputEmail1"]')
+
+    //by partial text match
+    page.locator(':text("Using")')
+
+    //by exact text match
+    page.locator(':text=is("Using the Grid")')
+})
+
