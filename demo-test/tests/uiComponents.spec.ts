3fcfbc748da4c8ce4025ed0e43e0aed046a6d395
Ali D <ali@phinion.com>
2024-02-15 19:17:36 +0100

    Make date picker more logical

--- demo-test/tests/uiComponents.spec.ts
+++ demo-test/tests/uiComponents.spec.ts
@@ -211,6 +211,23 @@ test("date pickers",async({page})=>{
 
   const calendarInputField = page.getByPlaceholder("Form Picker")
   await calendarInputField.click()
-  await page.locator('[class="day-cell ng-star-inserted"]').getByText('1',{exact:true}).click()
-  await expect(calendarInputField).toHaveValue("Feb 1, 2024")
+
+  let date = new Date()
+  date.setDate(date.getDate()+201)
+  const expectedDate= date.getDate().toString()
+  const expectedMonthShort=date.toLocaleString("En-US",{month:"short"});
+  const expectedMonthLong=date.toLocaleString("En-US",{month:"long"});
+  const expectedYear = date.getFullYear()
+  const dateToAssert=`${expectedMonthShort} ${expectedDate}, ${expectedYear}`
+
+  let calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+  const expectMonthAndYear = ` ${expectedMonthLong} ${expectedYear}`
+
+  while(!calendarMonthAndYear?.includes(expectMonthAndYear)){
+    await page.locator('in-calendar-pageable-navigation [data-name="chevron-right"]').click()
+    calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+  }
+  
+  await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate,{exact:true}).click()
+  await expect(calendarInputField).toHaveValue(dateToAssert)
 })
