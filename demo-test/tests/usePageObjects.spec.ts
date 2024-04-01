0dc2f81a63dcd8597c43ce867fa63b600611447e
Ali D <ali@phinion.com>
2024-04-01 17:10:06 +0200

    Get datepickers with using class

--- demo-test/tests/usePageObjects.spec.ts
+++ demo-test/tests/usePageObjects.spec.ts
@@ -1,7 +1,7 @@
 import {test,expect} from "@playwright/test"
 import { NavigationPage } from "../page-objects/navigationPage"
 import { formLayoutsPage } from "../page-objects/formLayoutsPage"
-
+import { DatepickerPage } from "../page-objects/datepickerPage"
 test.beforeEach(async({page})=>{
     await page.goto('http://localhost:4200/')
 })
@@ -15,7 +15,12 @@ test("navigate to form page", async({page})=>{
 test("parametrized methods",async({page})=>{
 const navigateTo = new NavigationPage(page)
 const onFormLayoutsPage = new formLayoutsPage(page)
+const onDatepickerPage = new DatepickerPage(page)
+
 await navigateTo.formLayoutsPage()
 await onFormLayoutsPage.submitGridFormCredentialsAndSelectOption("ali", "testpass", "Option 1")  
 await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckBox("ali", "ali@gmail.com",true)
+await navigateTo.datePickerPage()
+await onDatepickerPage.selectCommonDatePickerDateFromToday(5)
+await onDatepickerPage.selectRangeDatePickerDateFromToday(5,10)
 })
