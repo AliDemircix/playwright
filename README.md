f3ca66015121ffdab836ce0fd5177a32439b2e74
Ali D. <ali@phinion.com>
2024-06-04 14:38:35 +0200

    Retry failed tests

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # playwright
+
 - npm init playwright@latest //Install
 - npx playwright test // Start test
 - npx playwright show-report // Show test reports
@@ -10,7 +11,9 @@
 - npx playwright test --project=firefox --trace on // get details with screenshot
 - npx playwright test --project=firefox --debug // to run debug mode see step by step
 - to avoid dublicated test use beforeEach() hook
-- use more detailed selector like user behavior  page.getByRole('button',{name:/submit/i})
+- use more detailed selector like user behavior page.getByRole('button',{name:/submit/i})
 - add data-testid='SignIn' test attiribute in your app code to find page.getByTestId() locator
-- Click f8 to see tooltip details when you are in source tab 
+- Click f8 to see tooltip details when you are in source tab
 - Use extends to get inheritance from other class like class NavigationPage extends HelperBase
+- To change specific test retry number use this code in test block test.describe.configure({retries:2}) it should be in describe method
+- To clean database or something after retry use in the test as a second parameter testInfo testInfo.retry
--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,121 +1,133 @@
-import { test, expect, request } from "@playwright/test";
-import tags from "../test-data/tags.json";
+import { test, expect, request } from '@playwright/test';
+import tags from '../test-data/tags.json';
 
-test.beforeEach(async ({ page }) => {
-  await page.route("*/**/api/tags", async (route) => {
-    await route.fulfill({
-      body: JSON.stringify(tags),
+test.describe('Working with API', () => {
+  test.describe.configure({ retries: 2 });
+  test.beforeEach(async ({ page }) => {
+    await page.route('*/**/api/tags', async (route) => {
+      await route.fulfill({
+        body: JSON.stringify(tags),
+      });
     });
+
+    await page.goto('https://conduit.bondaracademy.com/');
+    await page.waitForTimeout(500);
   });
 
-  await page.goto("https://conduit.bondaracademy.com/");
-  await page.waitForTimeout(500);
-
-});
-
-test("has title", async ({ page }) => {
-
-  await page.route("**/api/articles*", async (route) => {
-    const response = await route.fetch();
-    const responseBody = await response.json();
-    responseBody.articles[0].title = "Phinion Projects";
-    responseBody.articles[0].description = "Phinion Projects Description";
-
-    await route.fulfill({
-      body: JSON.stringify(responseBody),
-    });
-  });
-
-  await page.getByText("Global Feed").click();
-  await expect(page.locator(".navbar-brand")).toHaveText("conduit");
-  await expect(page.locator("app-article-list h1").first()).toHaveText(
-    "Phinion Projects"
-  );
-  await expect(page.locator("app-article-list p").first()).toHaveText(
-    "Phinion Projects Description"
-  );
-});
-
-test("delete article", async ({ page, request }) => {
-  const response = await request.post(
-    "https://conduit-api.bondaracademy.com/api/users/login",
-    {
-      data: { user: { email: "alidemircix@gmail.com", password: "Test2024" } },
+  test('has title', async ({ page }, testInfo) => {
+    if (testInfo.retry) {
+      //do something like clear database etc.
     }
-  );
-  const responseBody = await response.json();
-  const accessToken = responseBody.user.token;
-  console.log(accessToken);
-  const articleResponse = await request.post(
-    "https://conduit-api.bondaracademy.com/api/articles/",
-    {
-      data: {
-        article: {
-          tagList: ["test"],
-          title: "Test Article",
-          description: "Test Article Description",
-          body: "Test Article Body",
+
+    await page.route('**/api/articles*', async (route) => {
+      const response = await route.fetch();
+      const responseBody = await response.json();
+      responseBody.articles[0].title = 'Phinion Projects';
+      responseBody.articles[0].description = 'Phinion Projects Description';
+
+      await route.fulfill({
+        body: JSON.stringify(responseBody),
+      });
+    });
+
+    await page.getByText('Global Feed').click();
+    await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+    await expect(page.locator('app-article-list h1').first()).toHaveText(
+      'Phinion Projects',
+    );
+    await expect(page.locator('app-article-list p').first()).toHaveText(
+      'Phinion Projects Description',
+    );
+  });
+
+  test('delete article', async ({ page, request }) => {
+    const response = await request.post(
+      'https://conduit-api.bondaracademy.com/api/users/login',
+      {
+        data: {
+          user: { email: 'alidemircix@gmail.com', password: 'Test2024' },
         },
       },
-      headers: {
-        Authorization: `Token ${accessToken}`,
+    );
+    const responseBody = await response.json();
+    const accessToken = responseBody.user.token;
+    console.log(accessToken);
+    const articleResponse = await request.post(
+      'https://conduit-api.bondaracademy.com/api/articles/',
+      {
+        data: {
+          article: {
+            tagList: ['test'],
+            title: 'Test Article',
+            description: 'Test Article Description',
+            body: 'Test Article Body',
+          },
+        },
+        headers: {
+          Authorization: `Token ${accessToken}`,
+        },
       },
-    }
-  );
-  expect(articleResponse.status()).toEqual(201);
-  await page.getByText("Global Feed").click();
-  await page.waitForTimeout(3000);
-  await page.getByText("Test Article").first().click();
-  await page.waitForTimeout(3000);
-  await page.getByRole("button", { name: "Delete Article" }).first().click();
+    );
+    expect(articleResponse.status()).toEqual(201);
+    await page.getByText('Global Feed').click();
+    await page.waitForTimeout(3000);
+    await page.getByText('Test Article').first().click();
+    await page.waitForTimeout(3000);
+    await page.getByRole('button', { name: 'Delete Article' }).first().click();
 
-  await expect(page.locator("app-article-list h1").first()).not.toContainText(
-    "Test Article"
-  );
-});
+    await expect(page.locator('app-article-list h1').first()).not.toContainText(
+      'Test Article',
+    );
+  });
 
-test("create article", async ({ page,request }) => {
-  await page.getByText("New Article").click();
-  await page
-    .getByRole("textbox", { name: "Article Title" })
-    .fill("Ali Test Article");
-  await page
-    .getByRole("textbox", { name: "What's this article about?" })
-    .fill("Test Article Description");
-  await page
-    .getByRole("textbox", { name: "Write your article (in markdown)" })
-    .fill("Test Article Body");
-  await page.getByText("Publish Article").click();
-  const articleResponse = await page.waitForResponse(
-    "https://conduit-api.bondaracademy.com/api/articles/"
-  );
-  const articleResponseBody = await articleResponse.json();
-  const slugId = articleResponseBody.article.slug;
+  test('create article', async ({ page, request }) => {
+    await page.getByText('New Article').click();
+    await page
+      .getByRole('textbox', { name: 'Article Title' })
+      .fill('Ali Test Article');
+    await page
+      .getByRole('textbox', { name: "What's this article about?" })
+      .fill('Test Article Description');
+    await page
+      .getByRole('textbox', { name: 'Write your article (in markdown)' })
+      .fill('Test Article Body');
+    await page.getByText('Publish Article').click();
+    const articleResponse = await page.waitForResponse(
+      'https://conduit-api.bondaracademy.com/api/articles/',
+    );
+    const articleResponseBody = await articleResponse.json();
+    const slugId = articleResponseBody.article.slug;
 
-  await expect(page.locator(".article-page h1").first()).toHaveText(
-    "Ali Test Article"
-  );
+    await expect(page.locator('.article-page h1').first()).toHaveText(
+      'Ali Test Article',
+    );
 
-  await page.getByText("Home").click();
-  await page.waitForTimeout(2000);
-  await page.getByText("Global Feed").click();
-  await page.waitForTimeout(2000);
-  await expect(page.locator("app-article-list h1").first()).toHaveText(
-    "Ali Test Article"
-  );
+    await page.getByText('Home').click();
+    await page.waitForTimeout(2000);
+    await page.getByText('Global Feed').click();
+    await page.waitForTimeout(2000);
+    await expect(page.locator('app-article-list h1').first()).toHaveText(
+      'Ali Test Article',
+    );
 
-   const response = await request.post(
-    "https://conduit-api.bondaracademy.com/api/users/login",
-    {
-      data: { user: { email: "alidemircix@gmail.com", password: "Test2024" } },
-    }
-  );
-  const responseBody = await response.json();
-  const accessToken = responseBody.user.token;
-  const deleteArticleResponse=await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${slugId}`,{
-    headers:{
-      Authorization: `Token ${accessToken}`
-    }
-  })
-  expect(deleteArticleResponse.status()).toEqual(204);
+    const response = await request.post(
+      'https://conduit-api.bondaracademy.com/api/users/login',
+      {
+        data: {
+          user: { email: 'alidemircix@gmail.com', password: 'Test2024' },
+        },
+      },
+    );
+    const responseBody = await response.json();
+    const accessToken = responseBody.user.token;
+    const deleteArticleResponse = await request.delete(
+      `https://conduit-api.bondaracademy.com/api/articles/${slugId}`,
+      {
+        headers: {
+          Authorization: `Token ${accessToken}`,
+        },
+      },
+    );
+    expect(deleteArticleResponse.status()).toEqual(204);
+  });
 });
